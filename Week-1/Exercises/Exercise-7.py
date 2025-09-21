ade48cf41c8f88866b2b00a873b9cd66e3eda6c9
zhang.valentina02@gmail.com <zhang.valentina02@gmail.com>
2025-09-21 22:44:01 +0200

    Added answers to Exercise-7

--- Week-1/Exercises/Exercise-7.py
+++ Week-1/Exercises/Exercise-7.py
@@ -7,3 +7,25 @@ should be programmed to keep guessing at it until it finds the number you are th
 At every step, add comments reflecting the logic of what the particular line of code is (supposed 
 to be) doing. 
 """
+print("Please think of a number between 1 and 100")
+
+low = 1 # set the lowest
+high = 100  # set the highest
+guess = (low + high) // 2  # the median as the first guess 
+
+while True: # loop
+    print(f"My guess is {guess}.") # give a guess
+    
+    answer = input("Is it correct (c), too low (l), or too high (h)? ").lower() # ask participant to give a feedback by giving an input, transformed into lower case
+    
+    if answer == 'c': # if participant indicated that the guess was correct then the loop breaks
+        print(f"The number was {guess}.")
+        break
+    elif answer == 'l': # or, if they indicate the guess was smaller than the true number
+        low = guess + 1 # set the lowest boundery to the guess + 1
+    elif answer == 'h': # or, if they indicate the guess was bigger than the true number
+        high = guess - 1  #set the lowest boundery to the guess - 1
+    else: # if they did not enter any of these letters, print the text again
+        print("Please type 'c', 'l', or 'h'.")
+    
+    guess = (low + high) // 2 # update the median/guess based on the new thresholds
